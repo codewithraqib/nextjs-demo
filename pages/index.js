e082a1c0c399bce4134502d2efa1a295f917384c
“Raqib <codewithraqib@gmail.com>
2025-02-18 08:58:12 +0530

    added multiple primary navs

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,6 @@ export default function Home() {
 
       <SecondaryNav />
       <PrimaryNav />
-      <PrimaryNav />
-      <PrimaryNav />
-      <PrimaryNav />
-      <PrimaryNav />
 
     </div>
   );
