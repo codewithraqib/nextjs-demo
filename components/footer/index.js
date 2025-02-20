5c430051182157d11048ad5662c412428b3c3ec3
anayatuu <anayatuu16@gmail.com>
2025-02-20 08:48:59 +0530

    anayat added benifits component

---
+++ components/footer/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+function Footer() {
+  const footerData = {
+    heading: [
+      {
+        id: 0,
+        title: "Resources",
+        subHeading: ["Find A Store", "Become A Member", "Send Us Feedback"],
+      },
+      {
+        id: 1,
+        title: "Help",
+        subHeading: [
+          "Get Help",
+          "Order Status",
+          "Delivery",
+          "Returns",
+          "Payment Options",
+          "Contact Us On Nike.com Inquiries",
+          "Contact Us On All Other Inquiries",
+        ],
+      },
+      {
+        id: 2,
+        title: "Company",
+        subHeading: [
+          "About Nike",
+          "News",
+          "Careers",
+          "Investors",
+          "Sustainability",
+          "Report a Concern",
+        ],
+      },
+    ],
+  };
+  return <></>;
+}
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -5,25 +5,26 @@ import styles from "../styles/Home.module.css";
 import Offers from "../components/offers";
 import HeroComponent from "../components/hero-component";
 import Gap from "../components/gap";
+import MemberBenifits from "../components/member-benifits";
+import Footer from "../components/footer";
 
 export default function Home() {
-
-
   let data = {
     id: 0,
     title: "CUSHIONING TO THE MAX",
     subTitle: "Coming 2.7",
     buttonText: "Notify Me",
-    image: "/assets/images/mainWrapper/main_img.png"
-  }
+    image: "/assets/images/mainWrapper/main_img.png",
+  };
 
   let data2 = {
     id: 1,
     title: "BUILT TO BE DIFFERENT",
-    subTitle: "More stability and better cushioning to unleash your explosive game",
+    subTitle:
+      "More stability and better cushioning to unleash your explosive game",
     buttonText: "Shop",
-    image: "/assets/images/mainWrapper/Screenshot25.png"
-  }
+    image: "/assets/images/mainWrapper/Screenshot25.png",
+  };
 
   return (
     <div className={styles.container}>
@@ -35,6 +36,8 @@ export default function Home() {
       <Latest />
       <Gap height={100} />
       <HeroComponent data={data2} />
+      <MemberBenifits />
+      <Footer />
     </div>
   );
 }
